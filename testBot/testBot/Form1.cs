84cf92774da6fa2788534b35f68b90999211b81a
mhjavad <jnekounam@yahoo.com>
2017-06-02 19:14:41 +0430

    new update 1.1

--- testBot/testBot/Form1.cs
+++ testBot/testBot/Form1.cs
@@ -19,11 +19,9 @@ namespace testBot
 {
     public partial class Form1 : Form
     {
-        private static readonly string botToken = "343393266:AAF1k2lXAai73noGmxuVRyW9f-JeD7s25MQ";
+        private static readonly string botToken = "343393266:AAG1SltZt3VaZNydfqNw4-i0p8w0DcXKlBo";
         private static List<MessageEntityType> entityType = new List<MessageEntityType>(new MessageEntityType[] { MessageEntityType.Url, MessageEntityType.Mention, MessageEntityType.TextLink, MessageEntityType.TextMention });
-        private static List<string> entityGuess = new List<string>(new string[] { "@","www", "http", ".com", ".me",".net",".co",".uk", ".org"});
-        private static WebClient webClient = new WebClient();
-        private static string address, result;
+        private static List<string> entityGuess = new List<string>(new string[] { "@", "www", "http", ".com", ".me", ".net", ".co", ".uk", ".org" });
         private static readonly TelegramBotClient bot = new TelegramBotClient(botToken);
         public Form1()
         {
@@ -40,43 +38,50 @@ namespace testBot
 
         private static async void botOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
         {
-            try
+            var message = messageEventArgs.Message;
+            var gChat = await bot.GetChatAsync(message.Chat.Id);
+            if(message.Text != null)
             {
-                var message = messageEventArgs.Message;
-                if(message.Text != null && message.Entities != null)
+                WebRequest req = WebRequest.Create("https://api.telegram.org/bot" + botToken + "/deleteMessage?chat_id=" + gChat.Id + "&message_id=" + message.MessageId);
+                req.UseDefaultCredentials = true;
+                var result = await req.GetResponseAsync();
+                req.Abort();
+            }
+            if (message.Text != null && message.Entities != null)
+            {
+                foreach (var entity in message.Entities)
                 {
-                    foreach (var entity in message.Entities)
+                    if (entityType.Contains(entity.Type))
                     {
-                        if (entityType.Contains(entity.Type))
-                        {
-                            address = "https://api.telegram.org/bot" + botToken + "/deleteMessage?chat_id=" + message.Chat.Id + "&message_id=" + message.MessageId;
-                            result = await webClient.DownloadStringTaskAsync(address);
-                        }
+                        WebRequest req = WebRequest.Create("https://api.telegram.org/bot" + botToken + "/deleteMessage?chat_id=" + gChat.Id + "&message_id=" + message.MessageId);
+                        req.UseDefaultCredentials = true;
+                        var result = await req.GetResponseAsync();
+                        req.Abort();
                     }
                 }
-                else if(message.Caption != null)
+            }
+            else if (message.Caption != null)
+            {
+                foreach (string srch in entityGuess)
                 {
-                    foreach (string srch in entityGuess)
+                    if (message.Caption.Contains(srch) == true && message.Caption.Contains("@yahoo") == false && message.Caption.Contains("@gmail") == false)
                     {
-                        if (message.Caption.Contains(srch) == true && message.Caption.Contains("@yahoo") == false && message.Caption.Contains("@gmail") == false)
-                        {
-                            address = "https://api.telegram.org/bot" + botToken + "/deleteMessage?chat_id=" + message.Chat.Id + "&message_id=" + message.MessageId;
-                            result = await webClient.DownloadStringTaskAsync(address);
-                        }
+                        WebRequest req = WebRequest.Create("https://api.telegram.org/bot" + botToken + "/deleteMessage?chat_id=" + gChat.Id + "&message_id=" + message.MessageId);
+                        req.UseDefaultCredentials = true;
+                        var result = await req.GetResponseAsync();
+                        req.Abort();
                     }
                 }
-
-                else if (message.Sticker != null)
-                {
-                    address = "https://api.telegram.org/bot" + botToken + "/deleteMessage?chat_id=" + message.Chat.Id + "&message_id=" + message.MessageId;
-                    result = await webClient.DownloadStringTaskAsync(address);
-                }
-
             }
-            catch (WebException)
+
+            else if (message.Sticker != null)
             {
-               
+                WebRequest req = WebRequest.Create("https://api.telegram.org/bot" + botToken + "/deleteMessage?chat_id=" + gChat.Id + "&message_id=" + message.MessageId);
+                req.UseDefaultCredentials = true;
+                var result = await req.GetResponseAsync();
+                req.Abort();
             }
+
         }
     }
 
